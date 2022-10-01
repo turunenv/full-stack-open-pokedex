41d0462fa9335b11ff3374f3f738a18dbb804bf6
turunenv <vili.turunen@aalto.fi>
2022-10-01 11:54:39 +0300

    testing hello world workflow

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000;
 
+//testing github actions first workflow
+
 app.use(express.static("dist"));
 
 app.listen(PORT, () => {
